f25298ce5268cf267e4112f36aaf88c1a4a96fae
Daniel Ross <69479756+notalexross@users.noreply.github.com>
2021-04-13 10:27:52 +0100

    refactor: move social links to .env

--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -8,9 +8,9 @@ export default function FooterContainer() {
         <Footer.Logo src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg" />
       </Footer.Left>
       <Footer.Right>
-        <Footer.Social iconClass="fab fa-github" href="https://github.com/notAlexRoss" />
-        <Footer.Social iconClass="fab fa-twitter" href="https://twitter.com/notAlexRoss" />
-        <Footer.Social iconClass="fas fa-palette" href="https://notAlexRoss.com" />
+        <Footer.Social iconClass="fab fa-github" href={process.env.REACT_APP_SOCIAL_GITHUB} />
+        <Footer.Social iconClass="fab fa-twitter" href={process.env.REACT_APP_SOCIAL_TWITTER} />
+        <Footer.Social iconClass="fas fa-palette" href={process.env.REACT_APP_SOCIAL_PORTFOLIO} />
       </Footer.Right>
     </Footer>
   )
